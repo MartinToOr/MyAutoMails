dae60e0cec2ec84c79c6ae87e9b9e531ec4e3984
MartinToOr <124706445+MartinToOr@users.noreply.github.com>
2025-07-12 18:33:42 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'automails',
+  host: process.env.DB_HOST || 'postgresql://neondb_owner:npg_9Sdfukrgl4ne@ep-damp-hat-a2skbnsr-pooler.eu-central-1.aws.neon.tech/MyAutoMails?sslmode=require&channel_binding=require',
+  user: process.env.DB_USER || 'neondb_owner',
+  password: process.env.DB_PASSWORD || 'npg_9Sdfukrgl4ne',
+  database: process.env.DB_NAME || 'MyAutoMails',
 });
 
 module.exports = pool;
