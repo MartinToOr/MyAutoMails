292459b65c75519bfb3283f4283486140c901b5c
MartinToOr <124706445+MartinToOr@users.noreply.github.com>
2025-07-12 19:03:07 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,9 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString:
-    process.env.DB_URL ||
-    'postgresql://neondb_owner:npg_9Sdfukrgl4ne@ep-damp-hat-a2skbnsr-pooler.eu-central-1.aws.neon.tech/MyAutoMails?sslmode=require&channel_binding=require',
+  connectionString: process.env.DB_URL,
 });
 
 module.exports = pool;
